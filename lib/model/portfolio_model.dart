5482c8a13930ef5742cce35a12acc3bb0a4b80b9
Bi <katariyadharmesh658@gmail.com>
2025-05-30 18:47:41 +0530

    portfolio section

--- lib/model/portfolio_model.dart
+++ lib/model/portfolio_model.dart
@@ -7,12 +7,16 @@ class PortfolioModel {
   String? description = '';
   String? buttonText = '';
   String? siteUrl = '';
-  RxBool isMoreExpanded = false.obs ;
+  RxBool isMoreExpanded = false.obs;
 
+  final List<String>? tags; // Add this for tags/chips
+  final bool? isFeatured; //
   PortfolioModel(
       {this.buttonText,
       this.description,
       this.title,
+      this.tags,
+      this.isFeatured,
       this.category,
       this.siteUrl,
       this.imagePath});
